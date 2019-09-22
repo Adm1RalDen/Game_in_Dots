c381e3a8fb6705677ef2e6e5d7b619fd937b018f
Adm1ralDen <games.zadrotina@gmail.com>
2019-09-22 19:41:17 +0300

    Updates

---
+++ precache-manifest.85f7d0f870c227e9d894845a4777db79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf7416bc1d7e8ee11de346ed19c4d3e",
+    "url": "/Game_in_Dots/index.html"
+  },
+  {
+    "revision": "bc03318fab00ba7c322c",
+    "url": "/Game_in_Dots/static/css/2.96121624.chunk.css"
+  },
+  {
+    "revision": "9b8cc1efa4a679d0639e",
+    "url": "/Game_in_Dots/static/css/main.28ed2f5e.chunk.css"
+  },
+  {
+    "revision": "bc03318fab00ba7c322c",
+    "url": "/Game_in_Dots/static/js/2.8c21cc46.chunk.js"
+  },
+  {
+    "revision": "9b8cc1efa4a679d0639e",
+    "url": "/Game_in_Dots/static/js/main.3db60763.chunk.js"
+  },
+  {
+    "revision": "efef8b1d4dc0b1eb182b",
+    "url": "/Game_in_Dots/static/js/runtime-main.d02e9600.js"
+  }
+]);
