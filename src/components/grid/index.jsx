58bef01bf75c45c5953111964bfd7844fd74060b
adm1ralden <games.zadrotina@gmail.com>
2019-10-02 21:40:39 +0300

    update bubling event

--- src/components/grid/index.jsx
+++ src/components/grid/index.jsx
@@ -5,14 +5,22 @@ import "./Grid.css";
 
 const Grid = props => {
   const handleClickToCell = event => {
-    console.log(event);
-    // if (props.Store.isPlay) {
-    //   let arr = props.Store.grid;
-    //   if (arr[i][j].classColor === "blue-cell") {
-    //     arr[i][j].classColor = "green-cell";
-    //     props.Store.setGrid(arr);
-    //   }
-    // }
+    let arr = event.target.attributes.class.nodeValue
+      .toString()
+      .split(" ")
+      .reverse()[0]
+      .split("/");
+    if (arr.length == 2) {
+      let [i, j] = arr
+
+      if (props.Store.isPlay) {
+        let arr = props.Store.grid;
+        if (arr[+i][+j].classColor === "blue-cell") {
+          arr[+i][+j].classColor = "green-cell";
+          props.Store.setGrid(arr);
+        }
+      }
+    }
   };
   return (
     <div className="grid-container">
@@ -23,7 +31,10 @@ const Grid = props => {
               <Row type="flex" justufy="center" key={index}>
                 {row.map((cell, j) => {
                   return (
-                    <Col key={j} className={`cell-in-row ${cell.classColor}`} />
+                    <Col
+                      key={j}
+                      className={`cell-in-row ${cell.classColor} ${index + '/' + j}`}
+                    />
                   );
                 })}
               </Row>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'mobx-react';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "mobx-react";
 
-import 'antd/dist/antd.css';
-import App from './App';
-import Store from './store/Store';
-
+import "antd/dist/antd.css";
+import App from "./App";
+import Store from "./store/Store"
 const stores = { Store };
-
 ReactDOM.render(
-    <Provider {...stores} >
-        <App />
-    </Provider>,
-    document.getElementById('root')
+  <Provider {...stores}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
